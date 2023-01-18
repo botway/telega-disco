9ec4a945e9004e37627f666dd3dc2bbf4ce2bc27
botway <botway@LAPTOP-36VKNH3J>
2023-01-18 13:52:49 +0100

    initial commit, MVP

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # telega-disco
-Auto mix youtube videos submitted through telegram bot by your community
+Auto mix youtube videos submitted through telegram bot by your community.
+
+Created this application for the Christmass party we hosted in 2022. Projected the videos on a large canvas, it was a lot of fun.
+
+*How to use:*
+1. Cretate your telegram bot as described here https://core.telegram.org/bots#how-do-i-create-a-bot
+2. Copy *Bot API Token* from API Token tab in the bot settings.
+3. Open .env file and paste the token between the qoutes into TELEGRAM_BOT_TOKEN="".
+4. Deploy the code to your hosting. For example, you can fork this repo and directly deploy it to glitch.com as described here https://help.glitch.com/kb/article/20-importing-code-from-github/ (note, that you will have to use glitch environmental variables insted of .env).
+5. Invite your users into the telegram bot and let them paste their favourite YouTube links into it.
+6. For the ease of use you can delete all default bot commands like /start
+7. Open the app in the browser, playback videos on the device of your choice and have fun.
+
+*Important:*
+
+If you will enable telegram webhook:
+* you won't be able to use any other telegram REST API endpoints.
+* if you will run it locally, you will need to configure an HTTP tunnel using ngrok agent https://ngrok.com/
+* the app should be hosted using HTTPS
+ 
+Only one bot instance can run at a time. If you want to run another service, you will need to create a new bot.
+
+If you want to skip the track, use NEXT button, which is faded in on mouse move or just use N key  
---
+++ index.js
@@ -0,0 +1,157 @@
+import express from "express";
+import got from "got";
+import fs from "fs";
+import path from "path";
+import { Telegraf } from "telegraf";
+import { EventEmitter } from 'events';
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import JSONdb from "simple-json-db";
+const db = new JSONdb("storage.json");
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const botApiKey = process.env.TELEGRAM_BOT_TOKEN;
+const telegramAPI = `https://api.telegram.org/bot${botApiKey}`;
+const bot = new Telegraf(botApiKey);
+
+const emitter = new EventEmitter();
+const app = express();
+
+let playlist = [];
+let updates = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// if the webhook is enabled, it won't be possible to use telegram REST API
+// domain should be the domain, where your app is hosted
+// app.use(await bot.createWebhook({ domain: example.com }));
+
+bot.on("text", ctx => {
+    const message = ctx.update.message;
+    addToPlaylist(message);
+    const curDB = db.JSON();
+    db.sync({ ...curDB });
+    emitter.emit("message", message);
+});
+bot.launch();
+
+app.get("/playlist-update", async function (req, res) {
+    res.set({
+        "Cache-Control": "no-cache",
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+    });
+    res.flushHeaders();
+
+    emitter.addListener("message", msg => {
+        if (db.has(msg.message_id)) {
+            const _msg = { user: msg.from.first_name, vidId: msg.text };
+            res.write("retry: 10000\n\n");
+            res.write(`data: ${JSON.stringify(_msg)}\n\n`);
+        }
+    });
+
+    res.connection.on("close", () => {
+        emitter.removeListener("message", () => console.log("listener removed"));
+        bot.stop();
+    })
+});
+
+app.get("/playlist", async (req, res) => {
+    const data = await getMessages();
+    playlist = setPlaylist(data);
+    res.json(playlist);
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+async function getMessages() {
+    const messages = await getBotMessages();
+    const prevDB = db.JSON();
+    messages.forEach((el) => {
+        addToPlaylist(el.message);
+    });
+    const curDB = db.JSON();
+    db.sync({ ...prevDB, ...curDB });
+    return { ...prevDB, ...curDB };
+}
+
+function addToPlaylist(message) {
+    if (message?.entities && message?.entities[0].type === "url") {
+        const data = {
+            username: message.from.username,
+            firstname: message.from.first_name,
+            date: message.date,
+            text: message.text,
+            vidId: handleMultipleLinks(message.text).map((l) => extractVidId(l)),
+        };
+        updates.push(data);
+        db.set(message.message_id, data);
+    }
+}
+
+function setPlaylist(data) {
+    const result = [];
+    Object.values(data).forEach((v) => {
+        if (v.vidId.length > 1) {
+            v.vidId.forEach((id) => {
+                if (id.length === 11) {
+                    result.push({
+                        username: v.username,
+                        firstname: v.first_name || '',
+                        vidId: id,
+                        date: v.date,
+                    });
+                }
+            });
+        } else {
+            if (v.vidId[0].length === 11) {
+                result.push({
+                    username: v.username,
+                    firstname: v.first_name || '',
+                    vidId: v.vidId[0],
+                    date: v.date,
+                });
+            }
+        }
+    });
+    return result;
+}
+
+async function getBotMessages() {
+    // only works without webhook enabled
+    const data = await got(`${telegramAPI}/getUpdates`);
+    // writeFile(data.body);
+    return JSON.parse(data.body).result;
+}
+
+function handleMultipleLinks(linksStr) {
+    return linksStr.split(/[\s,]+/);
+}
+
+function extractVidId(url) {
+    var match = url.match(
+        /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/
+    );
+    return match ? match["1"] : url;
+}
+
+function writeFile(data) {
+    fs.writeFile("results.json", data, function (err, data) {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+}
+
+const listener = app.listen(process.env.PORT || 2222, function () {
+    console.log("listening on port " + listener.address().port);
+});
